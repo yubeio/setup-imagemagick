61eb6cef98c0fc503a94143af2269fcc2fefb2a8
Gullit Miranda <gullitmiranda@gmail.com>
2020-08-02 18:44:02 -0300

    Check installed imagemagick installed version

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,46 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as toolCache from '@actions/tool-cache'
+
+// import {wait} from './wait'
 
 async function run(): Promise<void> {
+  checkPlatform()
+
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
+    const imageMagickVersion = core.getInput('imagemagick_version', {
+      required: true
+    })
+    // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
     core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
+
+    // Download and compile imagemagick if not already present
+    const toolPath = installedPath(imageMagickVersion)
+    core.debug(`toolPath: ${toolPath}`)
+
+    if (toolPath) {
+      core.debug('imagemagick already installed')
+    }
+
+    core.info(`Start setup imagemagick v${imageMagickVersion}`)
+
+    // await wait(parseInt(ms, 10))
     core.debug(new Date().toTimeString())
 
-    core.setOutput('time', new Date().toTimeString())
+    // core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     core.setFailed(error.message)
   }
+}
+
+function installedPath(version: string): string {
+  return toolCache.find('imagemagick', version)
+}
+
+function checkPlatform(): void {
+  if (process.platform !== 'linux')
+    throw new Error(
+      '@actions/setup-imagemagick only supports Ubuntu Linux at this time'
+    )
 }
 
 run()
